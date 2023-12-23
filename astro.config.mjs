08c9ae6bae334546f3facd7a4e8548a192ff2914
Marcos Corrochano Arroyo <mcorrochanoarroyo@gmail.com>
2023-12-23 18:54:59 +0100

    hotfix: icons import path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,6 @@ import mdx from '@astrojs/mdx'
 import expressiveCode from 'astro-expressive-code'
 import vercel from '@astrojs/vercel/serverless'
 
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-const _dirname = path.dirname(fileURLToPath(import.meta.url))
-
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
@@ -16,18 +12,5 @@ export default defineConfig({
     }),
     mdx()
   ],
-  vite: {
-    ssr: {
-      external: ['astro-heroicons']
-    },
-    resolve: {
-      alias: {
-        'astro-heroicons': path.resolve(
-          _dirname,
-          './node_modules/astro-heroicons'
-        )
-      }
-    }
-  },
   adapter: vercel()
 })
