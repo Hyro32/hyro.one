0b11fc84d007068c6bf38a5f3ba68d13646b1800
Marcos Corrochano Arroyo <mcorrochanoarroyo@gmail.com>
2023-12-23 17:58:45 +0100

    hotfix: vite path resolve

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import mdx from '@astrojs/mdx'
 import expressiveCode from 'astro-expressive-code'
 import vercel from '@astrojs/vercel/serverless'
 
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+const _dirname = path.dirname(fileURLToPath(import.meta.url))
+
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
@@ -15,6 +19,14 @@ export default defineConfig({
   vite: {
     ssr: {
       external: ['astro-heroicons']
+    },
+    resolve: {
+      alias: {
+        'astro-heroicons': path.resolve(
+          _dirname,
+          './node_modules/astro-heroicons'
+        )
+      }
     }
   },
   adapter: vercel()
