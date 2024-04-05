34d9e21b394654830c3cbe76f2eac8ac5c2f1582
Marcos Corrochano Arroyo <83825951+Mardroide@users.noreply.github.com>
2024-04-05 02:00:32 +0200

    Add Github pinned projects

---
+++ src/types/constants.ts
@@ -0,0 +1,4 @@
+export const REPOS_ENDPOINT =
+  'https://gh-pinned-repos.egoist.dev/?username=Mardroide'
+export const PAGE_TITLE = 'Mardroide - Portfolio'
+export const BLOG_TITLE = 'Mardroide - Blog'
---
+++ src/types/types.ts
@@ -0,0 +1,7 @@
+export interface Project {
+  repo: string
+  link: string
+  description: string
+  languageColor: string
+  language: string
+}
