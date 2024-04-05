99514bb216b8ab4b22feafa5e160eb3721687de6
Marcos Corrochano Arroyo <83825951+Mardroide@users.noreply.github.com>
2024-04-05 17:20:09 +0200

    Add new components and fix navigation

--- src/common/constants/constants.ts
+++ src/common/constants/constants.ts
@@ -2,3 +2,4 @@ export const REPOS_ENDPOINT =
   'https://gh-pinned-repos.egoist.dev/?username=Mardroide'
 export const PAGE_TITLE = 'Mardroide - Portfolio'
 export const BLOG_TITLE = 'Mardroide - Blog'
+export const SEARCH_TITLE = 'Mardroide - Search'
