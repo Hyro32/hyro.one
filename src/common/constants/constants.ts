bb6acb2f571aaaa4f6cf2b5e4416707aa224e9f3
Marcos Corrochano Arroyo <83825951+Mardroide@users.noreply.github.com>
2024-04-06 17:44:19 +0200

    Add responsive layout

--- src/common/constants/constants.ts
+++ src/common/constants/constants.ts
@@ -1,5 +1,7 @@
 export const REPOS_ENDPOINT =
   'https://gh-pinned-repos.egoist.dev/?username=Mardroide'
+export const GITHUB_URL =
+  'https://github.com/Mardroide/mardroide.dev/blob/main/src/content/blog/'
 export const PAGE_TITLE = 'Mardroide - Portfolio'
 export const BLOG_TITLE = 'Mardroide - Blog'
 export const SEARCH_TITLE = 'Mardroide - Search'
