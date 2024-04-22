04b2712f68e18410416aa818b5c724deade8ad70
Marcos Corrochano Arroyo <mcorrochanoarroyo@gmail.com>
2024-04-22 22:33:58 +0200

    Unnecesary adapter for static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,17 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import expressiveCode from 'astro-expressive-code'
-import vercel from '@astrojs/vercel/static'
-import tailwind from '@astrojs/tailwind'
-import icon from 'astro-icon'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import expressiveCode from 'astro-expressive-code';
+import vercel from '@astrojs/vercel/static';
+import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
-  integrations: [
-    expressiveCode({
-      themes: ['one-dark-pro']
-    }),
-    mdx(),
-    tailwind(),
-    icon()
-  ]
-})
+  integrations: [expressiveCode({
+    themes: ['one-dark-pro']
+  }), mdx(), tailwind(), icon()],
+  output: "server"
+});
