46f0ede9cf71c19703a0daa05d32a8424b2b15e9
Marcos Corrochano Arroyo <mcorrochanoarroyo@gmail.com>
2024-06-04 23:03:34 +0200

    astro-capo integration

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -6,9 +6,12 @@ const ui = {
     guestbook: 'guestbook',
     editOnGithub: 'Edit this post on GitHub',
     checkMyOrg: 'Check out my organization!',
-    navHome: 'Home',
-    projects: 'Projects',
-    people: 'People'
+    helpMinecraft: 'Contribute to my minecraft server!',
+    slogan: 'Studying Systems Engineering at UAH',
+    about1:
+      'I am a student of Systems Engineering at the University of Alcalá. I love programming and learning new things. I am currently learning new languages such as rust.',
+    about2:
+      'One of my dreams is to create a community of programmers who share their knowledge and experiences. Creating new projects, contributing and sharing knowledge is what motivates me to move forward.'
   },
   es: {
     home: 'inicio',
@@ -17,9 +20,12 @@ const ui = {
     guestbook: 'firmas',
     editOnGithub: 'Edita este post en GitHub',
     checkMyOrg: '¡Echa un vistazo a mi organización!',
-    navHome: 'Inicio',
-    projects: 'Proyectos',
-    people: 'Personas'
+    helpMinecraft: '¡Contribuye a mi servidor de minecraft!',
+    slogan: 'Estudiando Ingeniería de Sistemas en la UAH',
+    about1:
+      'Soy un estudiante de Ingeniería de Sistemas en la Universidad de Alcalá. Me encanta programar y aprender cosas nuevas. Actualmente estoy aprendiendo nuevos lenguajes como rust.',
+    about2:
+      'Uno de mis sueños es crear una comunidad de programadores que compartan sus conocimientos y experiencias. Crear nuevos proyectos, contribuir y compartir conocimientos es lo que me motiva a seguir adelante.'
   }
 }
 
