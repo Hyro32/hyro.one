7c8dbf61f94a200346e0765acb64553344b3fbcc
Marcos Corrochano Arroyo <83825951+Hyro32@users.noreply.github.com>
2024-07-28 20:33:18 +0200

    home page

--- src/common/constants/data.ts
+++ src/common/constants/data.ts
@@ -1,22 +1,132 @@
 export const socials = [
   {
-    icon: 'mingcute:github-line',
+    name: 'Github',
+    placeholder: 'github.com/Hyro32',
     url: 'https://github.com/Hyro32'
   },
   {
-    icon: 'mingcute:ins-line',
+    name: 'Instagram',
+    placeholder: '@marcos.corrochano',
     url: 'https://www.instagram.com/marcos.corrochano/'
   },
   {
-    icon: 'mingcute:linkedin-line',
-    url: 'https://www.linkedin.com/in/marcos-corrochano-arroyo-7103b2291/'
-  },
-  {
-    icon: 'mingcute:mail-line',
+    name: 'Email',
+    placeholder: 'mcorrochanoarroyo@gmail.com',
     url: 'mailto:mcorrochanoarroyo@gmail.com'
   },
   {
-    icon: 'mingcute:twitter-line',
+    name: 'X',
+    placeholder: '@HyroOne',
     url: 'https://twitter.com/HyroOne/'
+  }
+]
+
+export const apps = [
+  {
+    text: 'Visual Studio Code',
+    icon: 'https://code.visualstudio.com/favicon.ico'
+  },
+  {
+    text: 'Zed',
+    icon: 'https://zed.dev/favicon_white_32.png'
+  },
+  {
+    text: 'Intellij',
+    icon: 'https://www.jetbrains.com/favicon.ico'
+  },
+  {
+    text: 'Postman',
+    icon: 'https://www.postman.com/_ar-assets/images/favicon-1-48.png'
+  },
+  {
+    text: 'Table Plus',
+    icon: 'https://tableplus.com/resources/favicons/favicon-32x32.png'
+  }
+]
+
+export const tech = [
+  {
+    text: 'React',
+    icon: 'https://reactjs.org/favicon.ico'
+  },
+  {
+    text: 'Vercel',
+    icon: 'https://vercel.com/favicon.ico'
+  },
+  {
+    text: 'Vite',
+    icon: 'https://vitejs.dev/logo.svg'
+  },
+  {
+    text: 'Farmfe',
+    icon: 'https://farmfe.org/img/favicon.ico'
+  },
+  {
+    text: 'Tailwind',
+    icon: 'https://tailwindcss.com/favicons/favicon.ico'
+  },
+  {
+    text: 'Typescript',
+    icon: 'https://www.typescriptlang.org/favicon.ico'
+  },
+  {
+    text: 'Redis',
+    icon: 'https://redis.io/favicon.ico'
+  },
+  {
+    text: 'Node.js',
+    icon: 'https://nodejs.org/static/favicon.ico'
+  },
+  {
+    text: 'Docker',
+    icon: 'https://www.docker.com/favicon.ico'
+  },
+  {
+    text: 'Postgres',
+    icon: 'https://www.postgresql.org/media/img/about/press/elephant.png'
+  },
+  {
+    text: 'Nestjs',
+    icon: 'https://docs.nestjs.com/favicon.ico'
+  },
+  {
+    text: 'Java',
+    icon: 'https://www.java.com/favicon.ico'
+  },
+  {
+    text: 'Kotlin',
+    icon: 'https://kotlinlang.org/assets/images/favicon.svg'
+  },
+  {
+    text: 'Git',
+    icon: 'https://git-scm.com/favicon.ico'
+  },
+  {
+    text: 'Astro',
+    icon: 'https://astro.build/favicon.svg'
+  },
+  {
+    text: 'ElysiaJS',
+    icon: 'https://elysiajs.com/assets/elysia.svg'
+  },
+  {
+    text: 'Spigot',
+    icon: 'https://www.spigotmc.org/favicon.ico'
+  },
+  {
+    text: 'PaperMC',
+    icon: 'https://papermc.io/favicon.ico'
+  },
+  {
+    text: 'MongoDB',
+    icon: 'https://www.mongodb.com/favicon.ico'
+  },
+  {
+    text: 'Svelte',
+    icon: 'https://svelte.dev/favicon.png'
+  },
+  {
+    text: 'Turbo',
+    icon: 'https://turbo.build/images/product-icons/repo-dark-32x32.png'
   }
 ]
